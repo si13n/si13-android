bb40a9cf900566107ed003d0c89aff312e589b16
si13n <sergeydomin@gmail.com>
2026-07-17 13:50:37 +0200

    Implement basic bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
